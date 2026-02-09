f10c44896cf264f432e45f5af63d4666c8de3dc9
EricPuchalski <ericpuchalski12@gmail.com>
2026-02-09 18:27:34 -0300

    add gateway

---
+++ gateway-msc/src/main/java/com/ahorra_posadas/gateway_msc/GatewayMscApplication.java
@@ -0,0 +1,13 @@
+package com.ahorra_posadas.gateway_msc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayMscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayMscApplication.class, args);
+	}
+
+}
